51be8a6e0838a59bfbaa0651ae7351872283959a
openshwprojects <openshwprojects@gmail.com>
2023-04-12 15:37:11 +0200

    support both old and new names

--- vue/info.vue
+++ vue/info.vue
@@ -120,7 +120,7 @@
           "All","BK7231N","BK7231S","BK7231T","BL602", "XR809", "W800", "W600"
         ],
         selectedChipset: "All",
-        pinRoleNames: [
+        pinRoleNames_Old: [
           " ",
           "Relay",
           "Relay_n",
@@ -190,6 +190,78 @@
           "error",
           "error",
           "error"
+        ],
+        pinRoleNames: [
+            " ",
+            "Rel",
+            "Rel_n",
+            "Btn",
+            "Btn_n",
+            "LED",
+            "LED_n",
+            "PWM",
+            "WifiLED",
+            "WifiLED_n",
+            "Btn_Tgl_All",
+            "Btn_Tgl_All_n",
+            "dInput",
+            "dInput_n",
+            "TglChanOnTgl",
+            "dInput_NoPullUp",
+            "dInput_NoPullUp_n",
+            "BL0937SEL",
+            "BL0937CF",
+            "BL0937CF1",
+            "ADC",
+            "SM2135DAT",
+            "SM2135CLK",
+            "BP5758D_DAT",
+            "BP5758D_CLK",
+            "BP1658CJ_DAT",
+            "BP1658CJ_CLK",
+            "PWM_n",
+            "IRRecv",
+            "IRSend",
+            "Btn_NextColor",
+            "Btn_NextColor_n",
+            "Btn_NextDimmer",
+            "Btn_NextDimmer_n",
+            "AlwaysHigh",
+            "AlwaysLow",
+            "UCS1912_DIN",
+            "SM16703P_DIN",
+            "Btn_NextTemperature",
+            "Btn_NextTemperature_n",
+            "Btn_ScriptOnly",
+            "Btn_ScriptOnly_n",
+            "DHT11",
+            "DHT12",
+            "DHT21",
+            "DHT22",
+            "CHT8305_SDA",
+            "CHT8305_SCK",
+            "SHT3X_SDA",
+            "SHT3X_SCK",
+            "SoftSDA",
+            "SoftSCL",
+            "SM2235DAT",
+            "SM2235CLK",
+            "BridgeFWD",
+            "BridgeREV",
+            "Btn_SmartLED",
+            "Btn_SmartLED_n",
+            "DoorSnsrWSleep",
+            "DoorSnsrWSleep_nPup",
+            "BAT_ADC",
+            "BAT_Relay",
+            "TM1637_DIO",
+            "TM1637_CLK",
+            "BL0937SEL_n",
+            "DoorSnsrWSleep_pd",
+            "error",
+            "error",
+            "error",
+            "error",
         ],
         releases: [],
         latest: "", // read from github
@@ -402,7 +474,14 @@
         console.log('would save',this.channelTypes);
       },
       getRoleIndex(role){
-        return this.pinRoleNames.indexOf(role);
+        let res = this.pinRoleNames.indexOf(role);
+        if(res == -1){
+           res = this.pinRoleNames_Old.indexOf(role);
+        }
+        if(res == -1){
+          console.console.warn("WARNING: pin role " + role + " not found on tables!");
+        }
+        return res;
       },
       useDevice(){
         if (this.selectedDevice === null){ //No device was selected, keep current pin values
